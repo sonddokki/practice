f61ad63d37714f95ad0ee4be79399d0282542640
sonddokki <sonsharo@naver.com>
2023-08-28 18:55:41 +0900

    0828 SQL 과제 다시풀기

--- practice04 new.sql
+++ practice04 new.sql
@@ -85,8 +85,21 @@ WHERE job_id = 'ST_MAN';
 --단 조회결과는 급여의 내림차순으로 정렬되어 나타나야 합니다. 
 --조건절비교, 테이블조인 2가지 방법으로 작성하세요
 --(11건)
+SELECT  e.employee_id
+        ,e.first_name
+        ,e.salary
+        ,e.department_id
+FROM employees e, (SELECT  department_id
+                           ,max(salary) mas
+                   FROM employees
+                   GROUP BY department_id) d
+WHERE (e.department_id,e.salary) in ((d.department_id,d.mas))
+ORDER BY e.salary desc;
 
-
+SELECT  department_id
+        ,max(salary)
+FROM employees
+GROUP BY department_id; 
 
 
 --문제6.
@@ -94,17 +107,52 @@ WHERE job_id = 'ST_MAN';
 --연봉 총합이 가장 높은 업무부터 업무명(job_title)과 연봉 총합을 조회하시오 
 --(19건)
 
+SELECT  job.job_title  "업무명"
+        , em.sus       "연봉총합"
+FROM jobs job, (SELECT  job_id
+                      ,sum(salary) sus
+              FROM employees
+              GROUP BY job_id) em
+WHERE job.job_id =  em.job_id
+ORDER BY em.sus desc;
+              
 
-
+SELECT  job_id
+        ,sum(salary)    
+FROM employees
+GROUP BY job_id;
 
 --문제7.
 --자신의 부서 평균 급여보다 연봉(salary)이 많은 직원의 직원번호(employee_id), 이름(first_name)과 급여(salary)을 조회하세요 
 --(38건)
 
-
-
+SELECT  em.employee_id
+        ,em.department_id    -- 부서번호도 확인해보기
+        ,em.first_name
+        ,em.salary    
+FROM employees em, (SELECT  department_id
+                            ,avg(salary) avs
+                    FROM employees
+                    GROUP BY department_id) dep
+WHERE em.department_id = dep.department_id
+and em.salary > dep.avs
+ORDER BY  em.department_id asc;
+                
+                
+SELECT  department_id
+        ,avg(salary)
+FROM employees
+GROUP BY department_id;
 
 --문제8.
 --직원 입사일이 11번째에서 15번째의 직원의 사번, 이름, 급여, 입사일을 입사일 순서로 출력하세요
 
+SELECT
+    *
+FROM employees;
+
+
+SELECT hire_date
+FROM employees
+ORDER BY hire_date asc;
 
