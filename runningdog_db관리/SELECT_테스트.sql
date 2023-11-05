7b8bb76ae1d1332c3387bf1c8a5f99ec9dc10de7
sonddokki <sonsharo@naver.com>
2023-11-05 23:51:03 +0900

    12

--- runningdog_db관리/SELECT_테스트.sql
+++ runningdog_db관리/SELECT_테스트.sql
@@ -6,6 +6,9 @@ SELECT * FROM seq;
 SELECT * FROM users;
 SELECT * FROM trail;
 SELECT * FROM coords;
+SELECT * FROM dog;
+
+
 
 -- 산책로 하나 불러오는 쿼리문 
 SELECT  *
@@ -22,6 +25,15 @@ where co.type = 'trail'
 and tr.trailno = co.useno
 and co.useno = 3
 ORDER BY co.coordorder asc;
+
+-- 회원번호를 통해 회원의 강아지정보 가져오기
+SELECT * 
+FROM users us,dog do
+where us.userno = 1 
+and us.userno = do.userno;
+
+
+
 
 
 
